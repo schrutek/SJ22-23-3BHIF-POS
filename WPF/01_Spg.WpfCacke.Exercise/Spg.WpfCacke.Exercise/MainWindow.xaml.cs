9683165bf917e05ee9f178bbb8e9165565142180
schrutek <martin@schrutek>
2023-03-07 12:11:57 +0100

    WPF Cake Example added

---
+++ WPF/01_Spg.WpfCacke.Exercise/Spg.WpfCacke.Exercise/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spg.WpfCacke.Exercise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //this.GetType().GetProperties
+            //Colors
+            //object
+            InitializeComponent();
+
+            Person p = new Person("Martin", "Schrutek");
+
+            Type personType = p.GetType();
+
+            List<PropertyInfo> props = personType.GetProperties().ToList();
+
+        }
+    }
+}
---
+++ WPF/01_Spg.WpfCacke.Exercise/Spg.WpfCacke.Exercise/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spg.WpfCacke.Exercise
+{
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
